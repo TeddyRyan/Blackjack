9f246118ef5545e43fbef7de875e7c052a83309a
trfarr270@msn.com <trfarr270@msn.com>
2022-04-07 11:02:05 -0400

    Establishes blackjacks and declares winner

--- newblackjack.py
+++ newblackjack.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 class Card:
     def __init__(self, suit, value, card_value):
@@ -8,6 +9,10 @@ class Card:
 def print_cards(cards, reveal):
     for card in cards:
         print(card.value + " of " + card.suit)
+def __repr__(self):
+    print(deck)
+def clear():
+    os.system("clear")
 def blackjack_game(deck):
     
     #participant hands
@@ -15,41 +20,135 @@ def blackjack_game(deck):
     dealer_hand = []
     player_score = 0
     dealer_score = 0
+    player_blackjack = False
+    dealer_blackjack = False
+
+    #dealing dealer card 1
+    dealer_card = random.choice(deck)
+    dealer_hand.append(dealer_card)
+    deck.remove(dealer_card)
+
+    dealer_score += dealer_card.card_value
+
+    print("Dealer has: ")
+    print_cards(dealer_hand, True)
+
+    #dealing dealer card 2
+    dealer_card = random.choice(deck)
+    dealer_hand.append(dealer_card)
+    deck.remove(dealer_card)
+    
+    print("? of ?s")
+    print("Dealer's card score is: {score} + ?".format(score=dealer_score))
+
+    dealer_score += dealer_card.card_value
+
+    input("[hit enter to continue.]")
 
     #dealing player card 1
     player_card = random.choice(deck)
     player_hand.append(player_card)
     deck.remove(player_card)
 
+    player_score += player_card.card_value
 
     #dealing player card 2
     player_card = random.choice(deck)
     player_hand.append(player_card)
     deck.remove(player_card)
 
+    player_score += player_card.card_value
+    #player_score = 21
 
     print("Player has: ")
     print_cards(player_hand, True)
-    #print("Player's card score is: {score}".format())
+    print("Player's card score is: {score}".format(score=player_score))
+
+    if (len(player_hand) == 2) and (player_score == 21):
+        player_blackjack = True
+        print("*************************")
+        print("Player has blackjack!!!!!")
+        print("*************************")
+    if (len(player_hand) == 2) and (player_score > 21):
+        player_score -= 10
+
+    choice = ""
+    while choice != "s":
+        if player_blackjack == True:
+            input("[hit enter to continue.]")
+            break
+        choice = input("Do you want to hit or stand? [h/s]: ").lower()
+        #print(choice)
+        if choice == "h":
+            player_card = random.choice(deck)
+            player_hand.append(player_card)
+            deck.remove(player_card)
+
+            player_score += player_card.card_value
+            
+            print("Player now has:")
+            print_cards(player_hand, True)
+            print("Player's score is now: {score}".format(score=player_score))
 
 
-    dealer_card = random.choice(deck)
-    dealer_hand.append(dealer_card)
-    deck.remove(dealer_card)
+            if player_score > 21:
+                print("You lose!")
+                break
 
-   
+        elif choice == "s":
+            break
+        
+        else:
+            print("You must hit or stand. [h/s]")
 
-    dealer_card = random.choice(deck)
-    dealer_hand.append(dealer_card)
-    deck.remove(dealer_card)
+    if player_score <= 21:
+        print("Dealer reveals that he has:")
+        print_cards(dealer_hand, True)
+        print("Dealer's score is: {score}".format(score=dealer_score))
 
-    print("Dealer has: ")
-    print_cards(dealer_hand, True)
-
+        while dealer_score <= 21:
+            if (len(dealer_hand) == 2) and (dealer_score == 21):
+                dealer_blackjack = True
+                print("Dealer has blackjack!")
+                break
+            input("[hit enter to contine.]")
+            if dealer_score == 21:
+                print("Dealer stands!")
+                break
+            elif dealer_score > player_score:
+                print("Dealer stands!")
+                break
+            elif dealer_score < player_score:
+                print("Dealer hits!")
+                dealer_card = random.choice(deck)
+                dealer_hand.append(dealer_card)
+                deck.remove(dealer_card)
+                
+                dealer_score += dealer_card.card_value
+                
+                print("Dealer now has:")
+                print_cards(dealer_hand, True)
+                print("Dealer's score is: {score}".format(score=dealer_score))
+        
+        if dealer_score > 21:
+            print("Dealer busts!!!")  
+        if (dealer_score > 21) and (player_score <= 21):
+            print("Player wins!!!")
+        if (player_score > 21) and (dealer_score <= 21):
+            print("Dealer wins!!!")
+        if (player_score <= 21) and (dealer_score <= 21) and (player_score > dealer_score):
+            print("Player wins!!!")
+        if (player_score <= 21) and (dealer_score <= 21) and (dealer_score > player_score):
+            print("Dealer wins!")
+        if (player_score <= 21) and (dealer_score <= 21) and (dealer_score == player_score):
+            print("It's a tie!!!")
+    else:
+        print("Game over!!!")
     
+    input("[hit enter to clear.]")
+    clear()
 
     return deck
-
 if __name__ == '__main__':
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
     suits_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs": "\u2667", "Diamonds": "\u2662"}
