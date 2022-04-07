102a5f85c7e86872b9939fec5d5720cd20b2e1ab
trfarr270@msn.com <trfarr270@msn.com>
2022-04-07 12:19:29 -0400

    Fixes bust on Ace draw and distinguishes blackjacks from 21s

--- newblackjack.py
+++ newblackjack.py
@@ -42,6 +42,7 @@ def blackjack_game(deck):
     print("Dealer's card score is: {score} + ?".format(score=dealer_score))
 
     dealer_score += dealer_card.card_value
+    #dealer_score = 21
 
     input("[hit enter to continue.]")
 
@@ -60,18 +61,21 @@ def blackjack_game(deck):
     player_score += player_card.card_value
     #player_score = 21
 
-    print("Player has: ")
-    print_cards(player_hand, True)
-    print("Player's card score is: {score}".format(score=player_score))
-
+    
     if (len(player_hand) == 2) and (player_score == 21):
         player_blackjack = True
         print("*************************")
         print("Player has blackjack!!!!!")
         print("*************************")
+    
     if (len(player_hand) == 2) and (player_score > 21):
         player_score -= 10
 
+    print("Player has: ")
+    print_cards(player_hand, True)
+    print("Player's card score is: {score}".format(score=player_score))
+
+  
     choice = ""
     while choice != "s":
         if player_blackjack == True:
@@ -84,12 +88,17 @@ def blackjack_game(deck):
             player_hand.append(player_card)
             deck.remove(player_card)
 
+            #player_score = 20
+            #player_card.card_value = 11
             player_score += player_card.card_value
-            
+
+            if player_score > 21 and player_card.card_value == 11:
+                player_score -= 10
+                print("It worked!")
+
             print("Player now has:")
             print_cards(player_hand, True)
             print("Player's score is now: {score}".format(score=player_score))
-
 
             if player_score > 21:
                 print("You lose!")
@@ -100,16 +109,32 @@ def blackjack_game(deck):
         
         else:
             print("You must hit or stand. [h/s]")
-
+    
+    if (len(dealer_hand) == 2) and (dealer_score > 21):
+        dealer_score -= 10  
     if player_score <= 21:
         print("Dealer reveals that he has:")
         print_cards(dealer_hand, True)
         print("Dealer's score is: {score}".format(score=dealer_score))
 
+        skip_end = False
+
         while dealer_score <= 21:
             if (len(dealer_hand) == 2) and (dealer_score == 21):
                 dealer_blackjack = True
                 print("Dealer has blackjack!")
+                input("[hit enter to continue.]")
+            if (player_blackjack == True and dealer_blackjack == False):
+                print("Player has a blackjack and the dealer does not. Player wins!")
+                skip_end = True
+                break
+            if (player_blackjack == False and dealer_blackjack == True):
+                print("Dealer has a blackjack and the player does not. Dealer wins!")
+                skip_end = True
+                break
+            if (player_blackjack == True and dealer_blackjack == True):
+                print("Both players have blackjack. It's a tie!")
+                skip_end = True
                 break
             input("[hit enter to contine.]")
             if dealer_score == 21:
@@ -126,22 +151,26 @@ def blackjack_game(deck):
                 
                 dealer_score += dealer_card.card_value
                 
+                if dealer_score > 21 and dealer_card.card_value == 11:
+                    dealer_score -= 10
+
                 print("Dealer now has:")
                 print_cards(dealer_hand, True)
                 print("Dealer's score is: {score}".format(score=dealer_score))
         
-        if dealer_score > 21:
-            print("Dealer busts!!!")  
-        if (dealer_score > 21) and (player_score <= 21):
-            print("Player wins!!!")
-        if (player_score > 21) and (dealer_score <= 21):
-            print("Dealer wins!!!")
-        if (player_score <= 21) and (dealer_score <= 21) and (player_score > dealer_score):
-            print("Player wins!!!")
-        if (player_score <= 21) and (dealer_score <= 21) and (dealer_score > player_score):
-            print("Dealer wins!")
-        if (player_score <= 21) and (dealer_score <= 21) and (dealer_score == player_score):
-            print("It's a tie!!!")
+        if skip_end == False:
+            if dealer_score > 21:
+                print("Dealer busts!!!")  
+            if (dealer_score > 21) and (player_score <= 21):
+                print("Player wins!!!")
+            if (player_score > 21) and (dealer_score <= 21):
+                print("Dealer wins!!!")
+            if (player_score <= 21) and (dealer_score <= 21) and (player_score > dealer_score):
+                print("Player wins!!!")
+            if (player_score <= 21) and (dealer_score <= 21) and (dealer_score > player_score):
+                print("Dealer wins!")
+            if (player_score <= 21) and (dealer_score <= 21) and (dealer_score == player_score):
+                print("It's a tie!!!")
     else:
         print("Game over!!!")
     
@@ -151,7 +180,6 @@ def blackjack_game(deck):
     return deck
 if __name__ == '__main__':
     suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
-    suits_values = {"Spades":"\u2664", "Hearts":"\u2661", "Clubs": "\u2667", "Diamonds": "\u2662"}
     cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     cards_values = {"A": 11, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10}
     deck = []
